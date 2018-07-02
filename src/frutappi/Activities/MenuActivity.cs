58e29a0ac7ad3ea42df7b8682dfcb344b3d43457
bichito <you@example.com>
2018-07-02 17:16:27 -0500

    ...

--- src/frutappi/Activities/MenuActivity.cs
+++ src/frutappi/Activities/MenuActivity.cs
@@ -23,6 +23,7 @@ namespace frutappi.Activities
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_menu);
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
